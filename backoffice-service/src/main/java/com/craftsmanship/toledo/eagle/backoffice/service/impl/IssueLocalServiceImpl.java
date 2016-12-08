654798599b6a254c7742dee3aacf8e874a469824
Manuel de la Peña <manuel.delapenya@liferay.com>
2016-12-08 11:26:15 +0100

    Add service methods for adding/updating an issue

--- backoffice-service/src/main/java/com/craftsmanship/toledo/eagle/backoffice/service/impl/IssueLocalServiceImpl.java
+++ backoffice-service/src/main/java/com/craftsmanship/toledo/eagle/backoffice/service/impl/IssueLocalServiceImpl.java
@@ -16,7 +16,12 @@ package com.craftsmanship.toledo.eagle.backoffice.service.impl;
 
 import aQute.bnd.annotation.ProviderType;
 
+import com.craftsmanship.toledo.eagle.backoffice.model.Issue;
 import com.craftsmanship.toledo.eagle.backoffice.service.base.IssueLocalServiceBaseImpl;
+
+import com.liferay.portal.kernel.exception.PortalException;
+
+import java.util.Date;
 
 /**
  * The implementation of the issue local service.
@@ -39,4 +44,46 @@ public class IssueLocalServiceImpl extends IssueLocalServiceBaseImpl {
 	 *
 	 * Never reference this class directly. Always use {@link com.craftsmanship.toledo.eagle.backoffice.service.IssueLocalServiceUtil} to access the issue local service.
 	 */
+
+	public Issue addIssue(
+			String type, String title, String description, String summary,
+			String status, String picture)
+		throws PortalException {
+
+		long issueId = counterLocalService.increment();
+
+		Issue issue = issuePersistence.create(issueId);
+
+		issue.setIssueId(issueId);
+		issue.setType(type);
+		issue.setTitle(title);
+		issue.setDescription(description);
+		issue.setSummary(summary);
+		issue.setStatus(status);
+		issue.setVotes(0);
+		issue.setPicture(picture);
+
+		return issuePersistence.update(issue);
+	}
+
+	public Issue updateIssue(
+			long issueId, String type, String title, String description,
+			String summary, String status, String picture)
+		throws PortalException {
+
+		Issue issue = issuePersistence.findByPrimaryKey(issueId);
+
+		issue.setIssueId(issueId);
+		issue.setType(type);
+		issue.setTitle(title);
+		issue.setDescription(description);
+		issue.setSummary(summary);
+		issue.setStatus(status);
+		issue.setPicture(picture);
+
+		issue.setModifiedDate(new Date());
+
+		return issuePersistence.update(issue);
+	}
+
 }
